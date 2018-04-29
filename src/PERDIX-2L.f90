d9861653c79c277c8be777fa0c7f1bfd2778ac57
Hyungmin Jun <hmjeon@MIT.EDU>
2018-04-29 13:33:49 -0400

    completed

--- src/PERDIX-2L.f90
+++ src/PERDIX-2L.f90
@@ -2,7 +2,7 @@
 ! =============================================================================
 !
 ! PERDIX-2L v1.0
-! Last Updated : 04/27/2018, by Hyungmin Jun (hyungminjun@outlook.com)
+! Last Updated : 04/29/2018, by Hyungmin Jun (hyungminjun@outlook.com)
 !
 ! =============================================================================
 !
