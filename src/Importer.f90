0676f8f2a476cb0168596b126150cd87d758c2e8
Hyungmin <Hyungmin@HYUNGMIN-DESKTO>
2018-07-19 16:22:27 -0400

    completed

--- src/Importer.f90
+++ src/Importer.f90
@@ -332,7 +332,7 @@ subroutine Importer_SVG(prob, geom)
     integer :: i, j, n_poi, n_line, temp, ierr, length
     logical :: results
     character(200) :: path, fullname
-    character(len=100) :: text, word, xx, cx1, cx2, cy1, cy2
+    character(len=100) :: text, word, xx, cx1, cx2, cy1, cy2, c_line, c_point
     character(len=*), parameter :: search_str = "<line "
 
     ! Data structure for meshing
@@ -347,11 +347,11 @@ subroutine Importer_SVG(prob, geom)
     path     = "input/"//fullname
 
     ! Read number of points and faces
-    i = 0
+    n_line = 0
     if(prob.type_file == 'svg') then
         
         ! Check the number of lines
-        open(unit=1002, file=path, action="read")
+        open(unit=1002, file=trim(path), action="read")
         do
             ! Read line into character variable
             read(1002, "(a)", iostat = ierr) text
@@ -363,15 +363,17 @@ subroutine Importer_SVG(prob, geom)
 
             ! Found search string at beginning of line
             if(word == search_str) then
-                i = i + 1
+                n_line = n_line + 1
             end if
         end do
         close(unit=1002)
 
-        open(unit=1003, file=trim(path)//".geo", form="formatted")
-        write(1003, "(3i)"), i * 2, i, 0
-
-        i = 0
+        open(unit=1003, file="input/"//trim(prob.name_file)//".geo", form="formatted")
+        write(c_point, '(i)'), n_line * 2
+        write(c_line,  '(i)'), n_line
+        write(1003, "(a)"), adjustl(trim(c_point))//adjustl(trim(c_line))//adjustl(trim("0"))
+stop
+        n_line = 0
         open(unit=1002, file=path, action="read")
         do
             ! Read line into character variable
@@ -384,7 +386,7 @@ subroutine Importer_SVG(prob, geom)
 
             ! Found search string at beginning of line
             if(word == search_str) then
-                i = i + 1
+                n_line = n_line + 1
                 read(text, *), xx, xx, cx1, cy1, cx2, cy2
 
                 length = len_trim(cx1); xx = cx1(5:length-1); read(xx,*), x1
@@ -392,173 +394,19 @@ subroutine Importer_SVG(prob, geom)
                 length = len_trim(cx2); xx = cx2(5:length-1); read(xx,*), x2
                 length = len_trim(cy2); xx = cy2(5:length-1); read(xx,*), y2
 
-                print *, x1, y1
-                write(1003, "(i, 2f)"), 2*i-1, x1, y1
-                write(1003, "(i, 2f)"), 2*i,   x2, y2
+                write(1003, "(i, 2f)"), 2*n_line-1, x1, y1
+                write(1003, "(i, 2f)"), 2*n_line,   x2, y2
             end if
         end do
     end if
 
-    do j = 1, i
-        write(1003, "(3i)"), j, 2*j-1, 2*j
+    ! Construct the line connectivity
+    do i = 1, n_line
+        write(1003, "(3i)"), i, 2*i-1, 2*i
     end do
-
     close(unit=1003)
-stop
-    ! Make GEO file
 
-    ! Read GEO file
-
-    ! Boundary & internal mesh design
-    if(geom.n_face == 0) then
-        if(prob.type_file == 'geo') close(unit=1002)
-
-        write(0, "(a)"), "   * Converting lines to meshes by Shapely"
-        write(0, "(a)")
-
-        ! Convert to face meshes from lines
-        if(para_platform == "dev")   results = systemqq(trim("python tools/Shapely/Shapely.py")//" input/"//trim(fullname))
-        if(para_platform == "win")   results = systemqq(trim("python tools/Shapely/Shapely.py")//" input/"//trim(fullname))
-        !if(para_platform == "win")   results = systemqq(trim("tools\Shapely\Shapely.exe")//" input/"//trim(fullname))
-        if(para_platform == "mac")   results = systemqq(trim("python tools/Shapely/Shapely.py")//" input/"//trim(fullname))
-        if(para_platform == "linux") results = systemqq(trim("python tools/Shapely/Shapely.py")//" input/"//trim(fullname))
-
-        fullname = trim(prob.name_file)//trim("_shapely.geo")
-        open(unit=1002, file="input/"//trim(fullname), form="formatted")
-
-        ! Read number of points and faces
-        read(1002, *), geom.n_iniP, n_line, geom.n_face
-    end if
-
-    ! Exception module
-    if(geom.n_face == 0) then
-        close(1002)
-        write(0, "(a)")
-        write(0, "(a)"), "   +============================ E R R O R =============================+"
-        write(0, "(a)"), "   |                                                                    |"
-        write(0, "(a)"), "   |   The lines specifying the geometry are not connected.             |"
-        write(0, "(a)"), "   |                                                                    |"
-        write(0, "(a)"), "   +====================================================================+"
-        write(0, "(a)")
-        if(para_platform == "win") pause
-        stop
-    end if
-
-    ! Boundary design
-    if(geom.n_face == 1) then
-        write(0, "(a)")
-        write(0, "(a)"), "   Type the value (0.0 ~ 1.0) for the mesh spacing parameter [Enter]"
-        write(0, "(a)"), "   * The small value generates finer meshes"
-        read(*, *), p_mesh
-
-        if(p_mesh > 1.0d0) then
-            close(1002)
-            write(0, "(a)")
-            write(0, "(a)"), "   +============================ E R R O R =============================+"
-            write(0, "(a)"), "   |                                                                    |"
-            write(0, "(a)"), "   |   The mesh spacing parameter should be from 0.0 to 1.0             |"
-            write(0, "(a)"), "   |                                                                    |"
-            write(0, "(a)"), "   +====================================================================+"
-            write(0, "(a)")
-            if(para_platform == "win") pause
-            stop
-        end if
-
-        if(p_mesh >= 0.0d0 .and. p_mesh < 1.0) then
-            ! 0.0          x          1.0
-            ! 0.2          y          0.7
-            p_mesh = 0.2d0 + (0.7d0 - 0.2d0) * 1.0d0 * (p_mesh)
-            close(unit=1002)
-
-            if(para_platform == "dev") then
-                ! MATLAB - DistMesh
-                ! matlab -wait -nodisplay -nosplash -nodesktop -r
-                ! "addpath tools/DistMesh/src; addpath tools/DistMesh;
-                ! DistMesh('input/ex_des1_shapely.geo', 0.3)
-                results = systemqq("matlab -wait -nodisplay -nosplash -nodesktop -r "//&
-                    '"addpath tools/DistMesh/src; addpath tools/DistMesh; DistMesh('//&
-                    "'input/"//trim(fullname)//"',"//trim(Dble2Str(p_mesh))//')"')
-
-                ! Python - DistMesh
-                ! python tools/PyDistMesh/PyDistMesh.py input/ex_des1_shapely.geo 0.3
-                !results = systemqq(&
-                !trim("python tools/PyDistMesh/PyDistMesh.py input/")&
-                !    //trim(fullname)//' '//trim(Dble2Str(p_mesh)))
-            else if(para_platform == "win") then
-
-                ! MATLAB exe - DistMesh
-                ! tools\DistMesh\DistMesh.exe input\ex_des1_shapely.geo 0.3
-                !results = systemqq('tools\DistMesh\DistMesh.exe input\'&
-                !    //trim(fullname)//' '//trim(Dble2Str(p_mesh)))
-
-                results = systemqq("matlab -wait -nodisplay -nosplash -nodesktop -r "//&
-                    '"addpath tools/DistMesh/src; addpath tools/DistMesh; DistMesh('//&
-                    "'input/"//trim(fullname)//"',"//trim(Dble2Str(p_mesh))//')"')
-            else if(para_platform == "mac") then
-
-                ! Python - DistMesh
-                ! python tools/PyDistMesh/PyDistMesh.py input/ex_des1_shapely.geo 0.3
-                results = systemqq(&
-                    trim("python tools/PyDistMesh/PyDistMesh.py input/")&
-                    //trim(fullname)//' '//trim(Dble2Str(p_mesh)))
-            else if(para_platform == "linux") then
-
-                ! MATLAB - DistMesh
-                results = systemqq(&
-                    !"/usr/local/matlab/R2017a/bin/matlab wait -nodisplay -nosplash -nodesktop -noawt -r "//&
-                    "matlab wait -nodisplay -nosplash -nodesktop -noawt -r "//&
-                    '"addpath tools/DistMesh/src; addpath tools/DistMesh; DistMesh('//&
-                    "'input/"//trim(fullname)//"',"//trim(Dble2Str(p_mesh))//')"')
-            end if
-
-            fullname = trim(prob.name_file)//trim("_shapely_distmesh.geo")
-            open(unit=1002, file="input/"//trim(fullname), form="formatted")
-            read(1002, *), geom.n_iniP, n_line, geom.n_face
-        end if
-    end if
-
-    ! Read point data
-    allocate(geom.iniP(geom.n_iniP))
-    do i = 1, geom.n_iniP
-        read(1002, *), temp, geom.iniP(i).pos(1:2)
-        geom.iniP(i).pos(3) = 0.0d0
-
-        geom.iniP(i).pos(2) = geom.iniP(i).pos(2)
-    end do
-
-    ! Read face
-    allocate(geom.face(geom.n_face))
-    allocate(face_con(geom.n_face))
-    do i = 1, geom.n_face
-
-        face_con(i).cn(:) = 0
-        read(1002, *), temp, n_poi, face_con(i).cn(1:n_poi)
-
-        geom.face(i).n_poi = n_poi
-        allocate(geom.face(i).poi(n_poi))
-
-        do j = 1, n_poi
-            !geom.face(i).poi(j) = face_con(i).cn(n_poi-j+1)
-            geom.face(i).poi(j) = face_con(i).cn(j)
-        end do
-    end do
-
-    ! Deallocate memory and close file
-    deallocate(face_con)
-    close(unit=1002)
-
-    ! Delete temp file
-    if(para_platform == "win" .or. para_platform == "dev") then
-        results = systemqq(trim("del input\")//trim(prob.name_file)//trim("_shapely.geo"))
-    else
-        results = systemqq(trim("rm input/")//trim(prob.name_file)//trim("_shapely.geo"))
-    end if
-
-    if(para_platform == "win" .or. para_platform == "dev") then
-        results = systemqq(trim("del input\")//trim(prob.name_file)//trim("_shapely_distmesh.geo"))
-    else
-        results = systemqq(trim("rm input/")//trim(prob.name_file)//trim("_shapely_distmesh.geo"))
-    end if
+    call Importer_GEO(prob, geom)
 end subroutine Importer_SVG
 
 ! -----------------------------------------------------------------------------
